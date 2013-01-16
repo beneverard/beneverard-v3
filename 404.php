c8088a22b162b68fcdd7c5ed1b755de253d056b4
Ben Everard <hello@beneverard.co.uk>
2013-01-16 22:56:27 +0000

    Added sexy four oh four page

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php // front-page.php ?>
+
+<?php get_header(); ?>
+		
+	<div id="intro" class="align-center / pane pane-red">
+
+		<div class="pane-inner / align-center-item">
+
+			<h2>four oh four</h2>
+			
+		</div>
+
+	</div>
+
+<?php get_footer(); ?>
