05dde0d5b555aec444b70a2480075adfb9bb8de5
Ben Everard <hello@beneverard.co.uk>
2013-01-17 23:00:20 +0000

    Put Typekit async code at the top of the <head> element

--- header.php
+++ header.php
@@ -7,6 +7,17 @@
 		<meta charset='UTF-8'>
 		<meta name="author" content="Ben Everard" />
 		
+		<?php // TYPEKIT ?>
+		<script type="text/javascript">
+			(function() {
+				var config = {
+					kitId: 'hjv3ozo',
+					scriptTimeout: 3000
+				};
+				var h=document.getElementsByTagName("html")[0];h.className+=" wf-loading";var t=setTimeout(function(){h.className=h.className.replace(/(\s|^)wf-loading(\s|$)/g," ");h.className+=" wf-inactive"},config.scriptTimeout);var tk=document.createElement("script"),d=false;tk.src='//use.typekit.net/'+config.kitId+'.js';tk.type="text/javascript";tk.async="true";tk.onload=tk.onreadystatechange=function(){var a=this.readyState;if(d||a&&a!="complete"&&a!="loaded")return;d=true;clearTimeout(t);try{Typekit.load(config)}catch(b){}};var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(tk,s)
+			})();
+		</script>
+
 		<title><?php wp_title('&raquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
 
 		<?php // FAVICON ?>
@@ -19,17 +30,6 @@
 		<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/assets/css/screen.css">
 
 		<script src="<?php bloginfo('template_directory'); ?>/assets/js/libs/modernizr-custom.js"></script>
-
-		<?php // TYPEKIT ?>
-		<script type="text/javascript">
-			(function() {
-				var config = {
-					kitId: 'hjv3ozo',
-					scriptTimeout: 3000
-				};
-				var h=document.getElementsByTagName("html")[0];h.className+=" wf-loading";var t=setTimeout(function(){h.className=h.className.replace(/(\s|^)wf-loading(\s|$)/g," ");h.className+=" wf-inactive"},config.scriptTimeout);var tk=document.createElement("script"),d=false;tk.src='//use.typekit.net/'+config.kitId+'.js';tk.type="text/javascript";tk.async="true";tk.onload=tk.onreadystatechange=function(){var a=this.readyState;if(d||a&&a!="complete"&&a!="loaded")return;d=true;clearTimeout(t);try{Typekit.load(config)}catch(b){}};var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(tk,s)
-			})();
-		</script>
 
 		<?php wp_head(); ?>
 
