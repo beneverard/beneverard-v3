cbe2d8ad601a95930d3abdf574be795cb5c8d870
Ben Everard <hello@beneverard.co.uk>
2013-01-17 23:22:24 +0000

    Added loop to index.php template

--- index.php
+++ index.php
@@ -2,4 +2,17 @@
 
 <?php get_header(); ?>
 
+	<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+		<div class="align-center / pane pane-yello">
+
+			<div class="pane-inner / align-center-item">
+				<h2 class="tagline"><a href="<?php the_permalink(); ?>#content"><?php the_title(); ?></a></h2>
+				<h3><?php the_time('jS F Y'); ?></h3>
+			</div>	
+
+		</div>
+
+	<?php endwhile; ?>
+
 <?php get_footer(); ?>
