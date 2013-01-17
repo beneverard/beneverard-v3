77542c3f507788b312ac02b4b2d5463f3db8015f
Ben Everard <hello@beneverard.co.uk>
2013-01-17 21:19:21 +0000

    Changed URL to local scrollTo script

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-		<script src="https://raw.github.com/flesler/jquery.scrollTo/master/jquery.scrollTo.min.js"></script>
+		<script src="<?php bloginfo('template_directory'); ?>/assets/js/libs/jquery.scrollTo.min.js"></script>
 		<script src="<?php bloginfo('template_directory'); ?>/assets/js/libs/flexslider/jquery.flexslider-min.js"></script>
 		<script src="<?php bloginfo('template_directory'); ?>/assets/js/scripts.js"></script>
 
