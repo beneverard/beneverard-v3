769964eef62879654d382f883ae9e021b1ef4551
Ben Everard <hello@beneverard.co.uk>
2013-01-17 21:24:21 +0000

    Changed to use min version of scripts.js

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 		<script src="<?php bloginfo('template_directory'); ?>/assets/js/libs/jquery.scrollTo.min.js"></script>
 		<script src="<?php bloginfo('template_directory'); ?>/assets/js/libs/flexslider/jquery.flexslider-min.js"></script>
-		<script src="<?php bloginfo('template_directory'); ?>/assets/js/scripts.js"></script>
+		<script src="<?php bloginfo('template_directory'); ?>/assets/js/scripts.min.js"></script>
 
 		<?php wp_footer(); ?>	
 	
