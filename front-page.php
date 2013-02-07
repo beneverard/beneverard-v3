8c27a7b5b9283b76a2ac7434305cdda6a8884f05
Ben Everard <hello@beneverard.co.uk>
2013-02-07 16:05:12 +0000

    Added link around normal project slide header

--- front-page.php
+++ front-page.php
@@ -99,7 +99,7 @@
 
 								<div class="span_7 col / portfolio-content">
 
-									<h3><?php the_title(); ?></h3>
+									<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 									<p class="project-url"><a href="<?php the_field('project_url'); ?>"><?php the_field('project_url'); ?></a></p>
 
 									<?php the_excerpt(); ?>
